5c084d8636387dc708ba3c1bf500b26d432f35f8
Nowik434 <nowik434@gmail.com>
2022-02-15 18:51:59 +0100

    v1.2 added google analitics

--- pages/_document.js
+++ pages/_document.js
@@ -23,13 +23,6 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-                    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&display=swap" rel="stylesheet" />
-                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.14.0/devicon.min.css" />
-                    <link rel="manifest" href="/manifest.json" />
-                    <link rel="apple-touch-icon" href="/icon.png"></link>
-                    <meta name="theme-color" content="#212121" />
                     <script
                         async
                         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
@@ -46,6 +39,13 @@ class MyDocument extends Document {
                 `,
                         }}
                     />
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&display=swap" rel="stylesheet" />
+                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.14.0/devicon.min.css" />
+                    <link rel="manifest" href="/manifest.json" />
+                    <link rel="apple-touch-icon" href="/icon.png"></link>
+                    <meta name="theme-color" content="#212121" />
                 </Head>
                 <body>
                     <Main />

193998af6170795ec504b3774abbb9d348c6afd1
Nowik434 <nowik434@gmail.com>
2022-07-10 11:38:03 +0200

    v1.3 repaired grid

--- components/MainBar/MainBar.js
+++ components/MainBar/MainBar.js
@@ -1,60 +1,73 @@
-import React from 'react';
-import { Container, Div1, Div2, Title, Subtitle } from './MainBarStyle';
-import Image from 'next/image';
-import image from '../../assets/pn.png'
-import Development from '../../assets/app_development_SVG.svg'
+import React from "react";
+import { Container, Div1, Div2, Title, Subtitle } from "./MainBarStyle";
+import Image from "next/image";
+import image from "../../assets/pn.png";
+import Development from "../../assets/app_development_SVG.svg";
 
 const MainBar = (props) => {
+  return (
+    <Container
+      href={props.pathName}
+      theme={props.theme}
+      animate={{ opacity: 1 }}
+    >
+      {props.pathName === "/" ? (
+        <>
+          <Div1>
+            <Title>Paweł / Front End Developer</Title>
+            <Subtitle>
+              Jestem programistą hobbystą. Zajmuję się tworzeniem aplikacji od 4
+              lat, głównie z wykorzystaniem biblioteki React
+            </Subtitle>
+          </Div1>
+          <Div2>
+            <Image
+              src={image}
+              alt="Zdjęcie profilowe"
+              layout="fill"
+              objectFit="contain"
+              width={100}
+              className="profileImage"
+              style={{ top: "unset !important" }}
+            />
+          </Div2>
+        </>
+      ) : null}
 
-    return (
-        <Container href={props.pathName} theme={props.theme} animate={{ opacity: 1 }}>
-            {props.pathName === '/' ? (
-                <>
-                    <Div1>
-                        <Title>Paweł / Front End Developer</Title>
-                        <Subtitle>Jestem programistą hobbystą. Zajmuję się tworzeniem aplikacji od 4 lat, głównie z wykorzystaniem biblioteki React</Subtitle>
-                    </Div1>
-                    <Div2>
-                        <Image
-                            src={image}
-                            alt="Zdjęcie profilowe"
-                            layout="fill"
-                            objectFit="contain"
-                            width={100}
-                            className="profileImage"
-                        />
-                    </Div2>
-                </>
-            ) : null}
+      {props.pathName === "/technology" ? (
+        <>
+          <Div1>
+            <Title>Wykorzystywane technologie</Title>
+            <Subtitle>
+              Projekty tworzę głównie z wykorzystaniem biblioteki React i
+              frameworka Next.js.{" "}
+            </Subtitle>
+          </Div1>
+          <Div2 style={{ alignSelf: "center" }}>
+            <Image
+              src={Development}
+              alt="Technologie"
+              height={300}
+              width={300}
+              margin
+              className="profileImage"
+              style={{ margin: "20px" }}
+            />
+          </Div2>
+        </>
+      ) : null}
 
-            {props.pathName === '/technology' ? (
-                <>
-                    <Div1>
-                        <Title>Wykorzystywane technologie</Title>
-                        <Subtitle>Projekty tworzę głównie z wykorzystaniem biblioteki React i frameworka Next.js. </Subtitle>
-                    </Div1>
-                    <Div2 style={{ alignSelf: 'center' }}>
-                        <Image
-                            src={Development}
-                            alt="Technologie"
-                            height={300}
-                            width={300}
-                            margin
-                            className="profileImage"
-                            style={{ margin: '20px' }}
-                        />
-                    </Div2>
-                </>
-            ) : null}
-
-            {props.pathName === '/aboutme' ? (
-                <>
-                    <Div1>
-                        <Title>O Mnie</Title>
-                        <Subtitle>Projekty tworzę głównie z wykorzystaniem biblioteki React i frameworka Next.js. </Subtitle>
-                    </Div1>
-                    <Div2>
-                        {/* <Image
+      {props.pathName === "/aboutme" ? (
+        <>
+          <Div1>
+            <Title>O Mnie</Title>
+            <Subtitle>
+              Projekty tworzę głównie z wykorzystaniem biblioteki React i
+              frameworka Next.js.{" "}
+            </Subtitle>
+          </Div1>
+          <Div2>
+            {/* <Image
                             src={image}
                             alt="Zdjęcie profilowe"
                             layout="fill"
@@ -62,18 +75,18 @@ const MainBar = (props) => {
                             width={100}
                             className="profileImage"
                         /> */}
-                    </Div2>
-                </>
-            ) : null}
+          </Div2>
+        </>
+      ) : null}
 
-            {props.pathName === '/portfolio' ? (
-                <>
-                    <Div1>
-                        <Title>Moje projekty</Title>
-                        <Subtitle>Ostatnie projekty</Subtitle>
-                    </Div1>
-                    <Div2>
-                        {/* <Image
+      {props.pathName === "/portfolio" ? (
+        <>
+          <Div1>
+            <Title>Moje projekty</Title>
+            <Subtitle>Ostatnie projekty</Subtitle>
+          </Div1>
+          <Div2>
+            {/* <Image
                             src={image}
                             alt="Zdjęcie profilowe"
                             layout="fill"
@@ -81,12 +94,11 @@ const MainBar = (props) => {
                             width={100}
                             className="profileImage"
                         /> */}
-                    </Div2>
-                </>
-            ) : null}
-
-        </Container>
-    );
-}
+          </Div2>
+        </>
+      ) : null}
+    </Container>
+  );
+};
 
 export default MainBar;
--- components/MainBar/MainBarStyle.js
+++ components/MainBar/MainBarStyle.js
@@ -1,70 +1,71 @@
-import styled, { keyframes } from 'styled-components';
-import { motion } from "framer-motion"
-import { device } from '../../components/breakpoints';
+import styled, { keyframes } from "styled-components";
+import { motion } from "framer-motion";
+import { device } from "../../components/breakpoints";
 
 export const Container = styled(motion.div)`
-    grid-column: 2/7;
-    grid-row: ${(props) => props.theme.gridRow};
-    display: grid;
-    grid-template-columns: 2fr 1fr;
-    grid-template-rows: 1fr;
-    background-color: #212121;
-    border-radius: 9px;
-    transition: all 0.2s ease-out;
-    opacity: 0;
-    @media ${device.laptop} {
-        /* grid-row-end: ${(props) => props.theme.gridRowEndLaptop}; */
-    }
-    @media ${device.mobile} {
-        grid-column-start: 1;
-        /* grid-row-end: ${(props) => props.theme.gridRowEndMobile}; */
-        display: block;
-    }
+  grid-column: 2/7;
+  grid-row: ${(props) => props.theme.gridRow};
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  grid-template-rows: 1fr;
+  background-color: #212121;
+  border-radius: 9px;
+  transition: all 0.2s ease-out;
+  opacity: 0;
+  overflow: hidden;
+  @media ${device.laptop} {
+    /* grid-row-end: ${(props) => props.theme.gridRowEndLaptop}; */
+  }
+  @media ${device.mobile} {
+    grid-column-start: 1;
+    /* grid-row-end: ${(props) => props.theme.gridRowEndMobile}; */
+    display: block;
+  }
 `;
 
 Container.defaultProps = {
-    theme: {
-        gridRowEnd: 16,
-    }
-}
+  theme: {
+    gridRowEnd: 16,
+  },
+};
 
 export const Div1 = styled.div`
-    grid-column-start: 1;
-    grid-column-end: 1;
-    padding: 50px;
-    @media ${device.laptop} {
-        padding: 40px;
-    }
+  grid-column-start: 1;
+  grid-column-end: 1;
+  padding: 50px;
+  @media ${device.laptop} {
+    padding: 40px;
+  }
 `;
 
 export const Div2 = styled.div`
-    position:relative;
-    right: 0;
-    max-height: 420px;
-    overflow: hidden;
-    align-self: ${(props) => props.theme.alignSelf};
-    @media ${device.mobile} {
-        display: none;
-    }
+  position: relative;
+  right: 0;
+  max-height: 420px;
+  overflow: hidden;
+  align-self: ${(props) => props.theme.alignSelf};
+  @media ${device.mobile} {
+    display: none;
+  }
 `;
 
 export const Title = styled.h1`
-    color: #D5E242;
-    font-size: 60px;
-    @media ${device.laptop} {
-        font-size: 50px;
-    }
-    @media ${device.mobile} {
-        font-size: 30px;
-        text-align: center;
-    }
+  color: #d5e242;
+  font-size: 60px;
+  @media ${device.laptop} {
+    font-size: 50px;
+  }
+  @media ${device.mobile} {
+    font-size: 30px;
+    text-align: center;
+  }
 `;
 
 export const Subtitle = styled.h1`
-    color: #E0E0E0;
-    font-size: 26px;
-    font-weight: bold;
-    @media ${device.laptop} {
-        font-size: 20px;
-    }
+  color: #e0e0e0;
+  font-size: 26px;
+  font-weight: bold;
+  @media ${device.laptop} {
+    font-size: 20px;
+  }
 `;

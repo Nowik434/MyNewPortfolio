bd1da1e18cfcec7c8e1ddab8d0e849b87c03a279
Nowik434 <nowik434@gmail.com>
2022-07-10 11:15:01 +0200

    v1.3 repaired grid

--- components/Footer/FooterStyle.js
+++ components/Footer/FooterStyle.js
@@ -1,47 +1,48 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 import { motion } from "framer-motion";
-import { device } from '../../components/breakpoints';
+import { device } from "../../components/breakpoints";
 
 export const Container = styled(motion.div)`
-    display: inline-flex;
-    grid-template-columns: ${(props) => props.theme.gridTemplateColumns};
-    background-color: #212121;
-    grid-column: ${(props) => props.theme.gridColumn};
-    grid-row: ${(props) => props.theme.gridRow};
-    border-radius: 9px;
-    opacity: 0;
-    @media ${device.laptop} {
-        grid-row-start: ${(props) => props.theme.gridRowStartLaptop};
-        grid-row-end: ${(props) => props.theme.gridRowEndLaptop};
-    }
-    @media ${device.mobile} {
-        grid-column: ${(props) => props.theme.gridColumnMobile};
-        grid-row: ${(props) => props.theme.gridRowMobile};
-        display: ${(props) => props.theme.DisplayMobile};
-    }
+  display: inline-flex;
+  grid-template-columns: ${(props) => props.theme.gridTemplateColumns};
+  background-color: #212121;
+  grid-column: ${(props) => props.theme.gridColumn};
+  grid-row: ${(props) => props.theme.gridRow};
+  border-radius: 9px;
+  opacity: 0;
+  overflow: hidden;
+  @media ${device.laptop} {
+    grid-row-start: ${(props) => props.theme.gridRowStartLaptop};
+    grid-row-end: ${(props) => props.theme.gridRowEndLaptop};
+  }
+  @media ${device.mobile} {
+    grid-column: ${(props) => props.theme.gridColumnMobile};
+    grid-row: ${(props) => props.theme.gridRowMobile};
+    display: ${(props) => props.theme.DisplayMobile};
+  }
 `;
 
 Container.defaultProps = {
-    theme: {
-        gridTemplateColumns: '1fr 1fr',
-        gridColumnStart: 1,
-        gridColumnEnd: 4,
-        gridRowStart: 21,
-        gridRowEnd: 28,
-    }
-}
+  theme: {
+    gridTemplateColumns: "1fr 1fr",
+    gridColumnStart: 1,
+    gridColumnEnd: 4,
+    gridRowStart: 21,
+    gridRowEnd: 28,
+  },
+};
 
 export const Div = styled.div`
-    align-self: center;
-    img {
-        @media ${device.laptop} {
-        width: 350px;
-        }
-        @media ${device.laptopS} {
-        width: 300px;
-        }
-        @media ${device.mobile} {
-            width: 100%;
-        }
-    }   
+  align-self: center;
+  img {
+    @media ${device.laptop} {
+      width: 350px;
+    }
+    @media ${device.laptopS} {
+      width: 300px;
+    }
+    @media ${device.mobile} {
+      width: 100%;
+    }
+  }
 `;
